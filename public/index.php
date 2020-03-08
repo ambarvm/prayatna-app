b72b6a38b6a320de643f0a4dba546e8db37c4c4c
Ambar Mutha <ambar.ytl@gmail.com>
2020-03-08 19:58:37 +0530

    🚀 initial commit

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <?php include('../components/common-head.php') ?>
+    <link rel="stylesheet" href="./css/index.css">
+
+    <title>Prayatna App</title>
+</head>
+
+<body>
+    <main>
+        <h1>🏏Prayatna App</h1>
+    </main>
+
+    <!-- Scripts -->
+    <?php include('../components/common-scripts.php') ?>
+    <script src="./js/index.js"></script>
+</body>
+
+</html>
