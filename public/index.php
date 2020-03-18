78dcb8993e6d9e020a7bbbd1a5e5f5396951f3a9
nitink4472 <55016971+nitink4472@users.noreply.github.com>
2020-03-18 22:15:08 +0530

    Update index.php
    index.php has been updated with the addition of some HTML code.

--- public/index.php
+++ public/index.php
@@ -3,6 +3,9 @@
 <html lang="en">
 
 <head>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	<script src='https://kit.fontawesome.com/a076d05399.js'></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<?php include('../components/common-head.php') ?>
 	<link rel="stylesheet" href="./css/index.css">
 
@@ -10,6 +13,7 @@
 </head>
 
 <body>
+	<button onclick="topFunction()" id="myBtn" title="Go to top"><i class='fas fa-arrow-up' style='font-size:20px;color:white'></i></button>
 	<?php include('../components/navbar.php') ?>
 
 	<main>
