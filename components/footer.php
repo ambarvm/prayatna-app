3e8b9a73e57b4c19d7f05cbf8c2e03ecd20a6428
nitink4472 <55016971+nitink4472@users.noreply.github.com>
2020-03-18 22:02:04 +0530

    Update footer.php
    footer.php is updated.

--- components/footer.php
+++ components/footer.php
@@ -1,1 +1,18 @@
-<footer>footer works!!</footer>
+<footer class="footer">
+          <div class="container align">
+              <div class="col l4 offset-l2 s12">
+				<h5 class="white-text follow">Follow us on:</h5>
+                 <nav class="footer-nav">
+                 <a class="grey-text text-lighten-3" href="https://www.facebook.com/SportsIITBhilai/"><i class="fa fa-facebook-official" style="font-size:30px;color:blue"></i></a>&nbsp&nbsp&nbsp&nbsp
+		 		 <a class="grey-text text-lighten-3" href="https://twitter.com/IIT_Bhilai?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"><i class="fa fa-twitter" style="font-size:30px;color:blue"></i></a>&nbsp&nbsp&nbsp&nbsp
+		 		 <a class="grey-text text-lighten-3" href="https://www.instagram.com/iitbhilai/?hl=en"><i class="fa fa-instagram" style="font-size:30px;color:red"></i></a>
+                </nav>
+			  </div>         
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2020 Copyright at IIT Bhilai
+            <span class="grey-text text-lighten-4 right">All rights reserved.</span>
+            </div>
+          </div>
+        </footer>
