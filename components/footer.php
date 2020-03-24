5a825b79d5268b62793e2df9422f1c5d343a51ef
nitink4472 <55016971+nitink4472@users.noreply.github.com>
2020-03-25 02:34:10 +0530

    Fix #1: Add Footer and Nav-bar (#16)
    * Update footer.php
    
    footer.php is updated.
    
    * Updated index.php
    
    Index.php has been updated.
    
    * Update common.css
    
    * Update navbar.php
    
    * Update navbar.php
    
    * Update common.js
    
    * code cleaning🧹 and minor fixes
    - remove repeated cdn links
    - remove footer color
    - code formatting
    - move scroll to top button to footer file
    
    * remove repeated script tag
    
    Co-authored-by: Ambar Mutha <ambar.ytl@gmail.com>

--- components/footer.php
+++ components/footer.php
@@ -1,1 +1,23 @@
-<footer>footer works!!</footer>
+<button onclick="topFunction()" id="myBtn" title="Go to top">
+	<i class='fa fa-arrow-up' style='font-size:20px;color:white'></i>
+</button>
+
+<footer class="footer grey lighten-4">
+	<div class="container center">
+		<div class="col l4 offset-l2 s12">
+			<h5 class="follow">Follow us on</h5>
+			<div class="footer-nav">
+				<a class="grey-text text-lighten-3" href="https://www.facebook.com/SportsIITBhilai/"><i class="fa fa-facebook-official" style="font-size:30px;color:blue"></i></a>&nbsp&nbsp&nbsp&nbsp
+				<a class="grey-text text-lighten-3" href="https://twitter.com/IIT_Bhilai?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"><i class="fa fa-twitter" style="font-size:30px;color:blue"></i></a>&nbsp&nbsp&nbsp&nbsp
+				<a class="grey-text text-lighten-3" href="https://www.instagram.com/iitbhilai/?hl=en"><i class="fa fa-instagram" style="font-size:30px;color:red"></i></a>
+			</div>
+		</div>
+	</div>
+
+	<div class="footer-copyright">
+		<div class="container">
+			© 2020 Copyright at IIT Bhilai
+			<span class="right">All rights reserved.</span>
+		</div>
+	</div>
+</footer>
