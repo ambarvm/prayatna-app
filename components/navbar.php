4b59be1c70416f6db5c814242f1962edcb487bf3
Ambar Mutha <ambar.ytl@gmail.com>
2020-03-25 02:30:10 +0530

    code cleaning🧹 and minor fixes - remove repeated cdn links - remove footer color - code formatting - move scroll to top button to footer file

--- components/navbar.php
+++ components/navbar.php
@@ -1,26 +1,20 @@
-<nav class="white">
-     <div id="nav-wrapper">
-	<div id="container">
-           <a href="#" data-target="slide_out" class="sidenav-trigger black-text"><i class="material-icons">menu</i></a>
-           <a href="#" class="brand-logo center black-text">Prayatna</a>
-           
-        </div>
-     </div>
-</nav>
-<ul class="sidenav sidenav-fixed" id="slide_out">
-   <li><div class="user-view">
-          <div class="background">
-             
-          </div>
-       </div>
-   </li>
-<div>
-  
-   <li> <a href="index.html">Games</a></li>
-   <li> <a href="services.html">Sports</a></li>    
-   <li> <a href="contact.html">Photo Gallery</a></li>
-   <li> <a href="contact.html">Squads</a></li>
-   <li> <a href="contact.html">History</a></li>
-</div>
+<header>
+	<nav class="white">
+		<div id="nav-wrapper">
+			<div id="container">
+				<a href="#" data-target="slide_out" class="sidenav-trigger black-text"><i class="material-icons">menu</i></a>
+				<a href="#" class="brand-logo center black-text">Prayatna</a>
+			</div>
+		</div>
+	</nav>
+</header>
 
-   </ul>
+<ul class="sidenav sidenav-fixed" id="slide_out">
+	<div>
+		<li> <a href="index.html">Home</a></li>
+		<li> <a href="services.html">Sports</a></li>
+		<li> <a href="contact.html">Photo Gallery</a></li>
+		<li> <a href="contact.html">Squads</a></li>
+		<li> <a href="contact.html">History</a></li>
+	</div>
+</ul>
--- public/index.php
+++ public/index.php
@@ -3,13 +3,6 @@
 <html lang="en">
 
 <head>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-	<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	<script src='https://kit.fontawesome.com/a076d05399.js'></script>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="common.css"> 
 	<?php include('../components/common-head.php') ?>
 	<link rel="stylesheet" href="./css/index.css">
 
@@ -17,11 +10,10 @@
 </head>
 
 <body>
-	<button onclick="topFunction()" id="myBtn" title="Go to top"><i class='fas fa-arrow-up' style='font-size:20px;color:white'></i></button>
 	<?php include('../components/navbar.php') ?>
 
 	<main>
-		<h1>🏏Prayatna App</h1>
+		<h1 class="center">🏏Prayatna App</h1>
 	</main>
 
 	<?php include('../components/footer.php') ?>
@@ -29,7 +21,6 @@
 	<!-- Scripts -->
 	<?php include('../components/common-scripts.php') ?>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-	<script src="common.js"></script>
 	<script src="./js/index.js"></script>
 </body>
 
