08bfedce015b96ffcd3841a62144d3aaa7e65bc9
nitink4472 <55016971+nitink4472@users.noreply.github.com>
2020-04-18 22:04:02 +0530

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html>
 
 <html lang="en">
-
+<meta name = "viewport" content="width=device-width,initial-scale=1">      
+<link rel = "stylesheet" href = "https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+<script type = "text/javascript" src = "https://code.jquery.com/jquery-2.1.1.min.js"></script>           
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 <head>
 	<?php include('../components/common-head.php') ?>
 	<link rel="stylesheet" href="./css/index.css">
