3eb5263d80ae370fb32859ea64a29c7e0ff9a9f2
nitink4472 <55016971+nitink4472@users.noreply.github.com>
2020-04-18 22:02:02 +0530

    Update common.js

--- public/js/common.js
+++ public/js/common.js
@@ -18,3 +18,31 @@ function topFunction() {
 	document.body.scrollTop = 0;
 	document.documentElement.scrollTop = 0;
 }
+
+ $(document).ready(function(){
+    $('select').formSelect();
+  });
+$(document).ready(function(){
+    $('.datepicker').datepicker();
+  });
+$(document).ready(function(){
+    $('.timepicker').timepicker();
+  });
+
+document.getElementById("player1").style.display="none";
+document.getElementById("player2").style.display="none";
+function sports()
+{
+var x=document.getElementById("player").value;
+if(x=="3"||x=="2"||x=="4")
+{
+document.getElementById("player1").style.display="block";
+document.getElementById("player2").style.display="block";
+}
+else
+{
+document.getElementById("player1").style.display="none";
+document.getElementById("player2").style.display="none";
+}
+
+}
