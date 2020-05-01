0245c1dacc1669b167e53003d3e211ca75e8eba4
nitink4472 <55016971+nitink4472@users.noreply.github.com>
2020-05-01 17:50:13 +0530

    Added firebase.config.php

---
+++ firebase.config.php
@@ -0,0 +1,12 @@
+<script>
+	const firebaseConfig = {
+		apiKey: "AIzaSyCpYdQdfbGrcWXx-yalTmITpmQDbmPTjiI",
+		authDomain: "prayatna-app.firebaseapp.com",
+		databaseURL: "https://prayatna-app.firebaseio.com",
+		projectId: "prayatna-app",
+		storageBucket: "prayatna-app.appspot.com",
+		messagingSenderId: "216508513650",
+		appId: "1:216508513650:web:8121fc46a231932857b1eb",
+		measurementId: "G-KCDV0EY6LQ"
+	};
+</script>
