11318a1b503a6301aa7ee7e0648266fdd440b645
nitink4472 <55016971+nitink4472@users.noreply.github.com>
2020-06-06 20:31:12 +0530

    Fix #19: Add edit functionality to new match page (#20)
    * Update new-match.html
    
    * Update new-match.js
    
    * use doc id from url
    fix bugs and corner cases and improve code
    
    Co-authored-by: Ambar Mutha <ambar.ytl@gmail.com>

--- src/js/new-match.js
+++ src/js/new-match.js
@@ -1,7 +1,25 @@
 const db = firebase.firestore();
 
 const playerNamesElement = document.querySelector('#player-names');
+const formInputs = {
+	team1: document.getElementById('SelectTeam1'),
+	team2: document.getElementById('SelectTeam2'),
+	date: document.getElementById('date'),
+	time: document.getElementById('time'),
+	sport: document.getElementById('sport-select'),
+	player1: document.getElementById('player1'),
+	player2: document.getElementById('player2'),
+};
+
 let includePlayerNames = false;
+
+const url = new URL(window.location.href);
+const docID = url.searchParams.get('id');
+let editMode = !!docID;
+
+if (docID) {
+	loadFormData(docID);
+}
 
 function onSportChange(event) {
 	const individualSports = ['badminton', 'chess', 'table tennis'];
@@ -13,44 +31,66 @@ function onSportChange(event) {
 	} else {
 		playerNamesElement.style.display = 'none';
 		includePlayerNames = false;
+		formInputs.player1.value = '';
+		formInputs.player2.value = '';
+		M.updateTextFields();
 	}
 }
 
-function addEvent() {
-	db.collection('events')
-		.add({
-			team1: document.getElementById('SelectTeam1').value,
-			team2: document.getElementById('SelectTeam2').value,
-			date: document.getElementById('date').value,
-			time: document.getElementById('time').value,
-			sport: document.getElementById('sport-select').value,
-			player1: includePlayerNames
-				? document.getElementById('player1').value
-				: null,
-			player2: includePlayerNames
-				? document.getElementById('player2').value
-				: null,
-		})
-		.then(function (docRef) {
-			console.log('Document written with ID: ', docRef.id);
-			M.toast({
-				html: 'Form is submitted',
-				classes: 'rounded',
-				displayLength: 1000,
-			});
-			document.getElementById('form').reset();
-		})
-		.catch(function (error) {
-			console.error('Error adding document: ', error);
-			M.toast({
-				html: 'Something Went Wrong',
-				classes: 'rounded',
-				displayLength: 1000,
-			});
+async function addEvent() {
+	const formData = {};
+
+	for (const key in formInputs) {
+		if (formInputs.hasOwnProperty(key)) {
+			formData[key] = formInputs[key].value;
+		}
+	}
+
+	try {
+		const events = db.collection('events');
+
+		if (editMode) {
+			await events.doc(docID).set(formData);
+			console.log(`Doc updated at ${docID}`);
+		} else {
+			const docRef = await events.add(formData);
+			console.log(`Doc added at ${docRef.id}`);
+		}
+
+		M.toast({
+			html: 'Form is submitted',
+			classes: 'rounded',
+			displayLength: 1000,
 		});
+		document.getElementById('form').reset();
+	} catch (error) {
+		console.error('Error adding document: ', error);
+
+		M.toast({
+			html: 'Something Went Wrong',
+			classes: 'rounded',
+			displayLength: 1000,
+		});
+	}
+}
+
+async function loadFormData(docID) {
+	const docRef = db.collection('events').doc(docID);
+
+	const doc = await docRef.get();
+
+	if (doc.exists) {
+		for (const key in formInputs) {
+			if (formInputs.hasOwnProperty(key)) {
+				formInputs[key].value = doc.get(key);
+			}
+		}
+		M.AutoInit();
+		M.updateTextFields();
+	} else {
+		editMode = false;
+	}
 }
 
 document.querySelector('#submit-btn').addEventListener('click', addEvent);
-document
-	.querySelector('#sport-select')
-	.addEventListener('change', onSportChange);
+formInputs.sport.addEventListener('change', onSportChange);
