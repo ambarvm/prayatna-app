53cee9e80977c2c726636db95c9323f0672b2a2a
nitink4472 <55016971+nitink4472@users.noreply.github.com>
2020-06-06 17:10:07 +0530

    Update new-match.js

--- src/js/new-match.js
+++ src/js/new-match.js
@@ -54,3 +54,54 @@ document.querySelector('#submit-btn').addEventListener('click', addEvent);
 document
 	.querySelector('#sport-select')
 	.addEventListener('change', onSportChange);
+
+M.AutoInit();
+
+function editid()
+{
+	
+	var e=document.getElementById("checkid").value;
+	let db=firebase.firestore();
+	var docRef = db.collection("events").doc("e");
+
+	docRef.get().then(function(doc) {
+    	if (doc.exists) 
+    	{
+		document.getElementById("SelectTeam1").value=doc.get("team1");
+		document.getElementById("SelectTeam2").value=doc.get("team2");
+		document.getElementById("date").value=doc.get("date");
+		document.getElementById("time").value=doc.get("time");
+		document.getElementById("sport-select").value=doc.get("sport");
+		document.getElementById("player1").value=doc.get("player1");
+		document.getElementById("player2").value=doc.get("player2");
+		M.AutoInit();
+		
+		var a,b,c,d,e,f,g;
+		
+		a=document.getElementById("SelectTeam1").value;
+		b=document.getElementById("SelectTeam2").value;
+		c=document.getElementById("date").value;
+		d=document.getElementById("time").value;
+		e=document.getElementById("sport-select").value;
+		f=document.getElementById("player1").value;
+		g=document.getElementById("player2").value;
+		
+		db.collection("events").doc("e").update({
+			team1:a,
+			team2:b,
+			date:c,
+			time:d,
+			sport:e,
+			player1:f,
+			player2:g
+		})
+		
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+	}).catch(function(error) {
+    console.log("Error getting document:", error);
+	});
+}
+document.querySelector('#edit').addEventListener('click', editid);
